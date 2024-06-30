e68e8278dac5e3cd4a86f3d812c421de7492f0e2
cosmingeorge8 <60880813+cosmingeorge8@users.noreply.github.com>
2024-06-30 04:43:42 +0200

    added getSignetUrl

--- src/services/upload-service.ts
+++ src/services/upload-service.ts
@@ -7,7 +7,9 @@ import {S3} from "aws-sdk";
 export class UploadService {
 
     private static readonly logger = new Logger(UploadService.name);
-    private static s3 = new S3();
+    private static s3 = new S3({
+        signatureVersion: 'v4',
+    });
 
     static async uploadFile(file: UploadedFile): Promise<string> {
         this.logger.info('Uploading image');
@@ -35,6 +37,29 @@ export class UploadService {
         } catch (e) {
             this.logger.error(`Error uploading image to S3 ${e}`);
             throw new FileUploadError('Error uploading image');
+        }
+    }
+
+    static getSignedUrl(image: string) {
+        this.logger.info(`Getting signed URL for image ${image}`);
+        //Get the key from the image URL
+        const key = image.split('/').slice(-1)[0];
+        //Get a signed URL for the image
+        try {
+            this.logger.debug(`Setting region to ${process.env.S3_BUCKET_REGION}`)
+            this.s3.config.region = process.env.S3_BUCKET_REGION;
+            this.logger.debug(`S3 bucket region set successfully ${this.s3.config.region}`)
+            const result = this.s3.getSignedUrl('getObject', {
+                Bucket: process.env.S3_BUCKET_NAME as string,
+                Key: key,
+                Expires: 60 * 60,
+            });
+            this.logger.info(`Signed URL for image ${image} generated successfully`);
+            this.logger.debug(`Signed URL: ${result}`);
+            return result;
+        } catch (e) {
+            this.logger.error(`Error getting signed URL for image ${e}`);
+            throw new FileUploadError('Error getting signed URL');
         }
     }
 }
