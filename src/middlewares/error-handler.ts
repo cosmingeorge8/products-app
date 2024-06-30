6ed1891848006bd6185279b2fab27ad27cbc1ece
cosmingeorge8 <60880813+cosmingeorge8@users.noreply.github.com>
2024-06-30 05:00:35 +0200

    added extra logging in errorHandler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,12 +1,16 @@
 // middlewares/errorHandler.ts
 import { Request, Response, NextFunction } from 'express';
 import {CustomError} from "../errors/custom-error";
+import {Logger} from "../logger/logger";
+
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    const logger: Logger = new Logger('ErrorHandler');
     if (err instanceof CustomError) {
+        logger.error(`Error occurred: ${err.message}, throwing status code: ${err.statusCode}`);
         return res.status(err.statusCode).json({ message: err.message });
     }
 
-    console.error(err.stack);
+    logger.error(`An unexpected error occurred: ${err.message}`);
     res.status(500).json({ message: 'An unexpected error occurred' });
 };
